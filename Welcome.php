213af8b44afe1e2771abfbefcdfa5715b8b1b403
Piebe <piebehendrikk@gmail.com>
2020-05-11 15:42:16 +0200

    #2

---
+++ Welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+<body>
+<nav class="navtop">
+</nav>
+<div class="wrapper_welcome">
+    <span class="text1">Welcome</span>
+</div>
+</body>
