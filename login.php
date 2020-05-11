eebae50f9bda3fc9d1bd686333338458ef902edd
Piebe <piebehendrikk@gmail.com>
2020-05-11 16:13:49 +0200

    #3

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Log in</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+<body>
+<nav class="navtop">
+</nav>
+</body>
